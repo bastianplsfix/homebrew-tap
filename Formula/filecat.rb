4e84d5441b640ee7a69bc2c038053973844d8d26
bastianplsfix <sebastian.nomme@icloud.com>
2026-01-21 13:30:18 +0100

    Fix SHA256 hashes

--- Formula/filecat.rb
+++ Formula/filecat.rb
@@ -7,19 +7,19 @@ class Filecat < Formula
   on_macos do
     on_intel do
       url "https://github.com/bastianplsfix/filecat/releases/download/v#{version}/filecat-darwin-x86_64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_DARWIN_X86_64"
+      sha256 "71512e2de0505caba2dd4905c748d23ed26a756670130d2d08bbb9ab84d58832"
     end
 
     on_arm do
       url "https://github.com/bastianplsfix/filecat/releases/download/v#{version}/filecat-darwin-aarch64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_DARWIN_AARCH64"
+      sha256 "fce3802e0c55314fafe6c0cd4da012a17708c40d57160ebe924a70598beeacd4"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/bastianplsfix/filecat/releases/download/v#{version}/filecat-linux-x86_64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_LINUX_X86_64"
+      sha256 "857a9c911eaab33f8dc71a74aa1c8ae5c7205c994f9d52f09249c672cd8ff8de"
     end
   end
 
